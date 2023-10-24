762466796dab1fe244ae39f01529f9d10ec20519
lian <lian.dy220@gmail.com>
2023-10-24 22:39:46 +0900

    day5

---
+++ src/main/kotlin/com/group/libraryapp/LibraryAppApplication.kt
@@ -0,0 +1,10 @@
+package com.group.libraryapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryAppApplication
+fun main(args: Array<String>) {
+    runApplication<LibraryAppApplication>(*args)
+}
--- src/main/kotlin/com/group/libraryapp/dto/book/BookRequest.kt
+++ src/main/kotlin/com/group/libraryapp/dto/book/BookRequest.kt
@@ -1,5 +1,5 @@
 package com.group.libraryapp.dto.book
 
 data class BookRequest(
-        val name: String
+    val name: String,
 )
